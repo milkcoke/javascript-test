708379943d70d9223c9788420a62b90260cd4621
milkcoke <mbh023@gmail.com>
2023-03-02 14:38:39 +0900

    first commit

---
+++ test/bigint.test.js
@@ -0,0 +1,22 @@
+
+// `new` 키워드 없이 생성
+
+
+describe('Bigint test', ()=>{
+  test('Not equal safe max', ()=>{
+    const bigNum = BigInt(9007199254741091) // MAX_SAFE_INTEGER + 100, 9007199254741092n
+    expect(bigNum).not.toBe(Number.MAX_SAFE_INTEGER + 100) // 9007199254741092n 1 오차 발생
+  })
+})
+
+// Number.MAX_SAFE_INTEGER : 9007199254740991
+
+
+// const strBigNum = BigInt('9007199254741091')
+// console.log(strBigNum) // 9007199254741091n
+//
+// const bigTrue = BigInt(true)
+// console.log(bigTrue) // 1n
+//
+// const bigFalse = BigInt(false)
+// console.log(bigFalse) // 0n
---
+++ test/jest.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  moduleFileExtensions: [
+    'js'
+  ],
+  rootDir: '.',
+  testRegex: '.*|(\\\\.|/)(test|spec)\\\\.js$',
+  collectCoverageFrom: [
+    '**/*.(t|j)s'
+  ],
+  testEnvironment: 'node'
+}
